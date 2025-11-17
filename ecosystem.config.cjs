061ff3ca200cc72b33b487de1130f11e86bae8e4
Bagus <25834188+contactjavas@users.noreply.github.com>
2025-11-18 00:56:14 +0700

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'secure-qr-scanner-web',
-    script: 'node_modules/vinxi/dist/cli.mjs',
+    script: 'node_modules/vinxi/bin/cli.mjs',
     args: 'start',
     env_production: {
       NODE_ENV: 'production',
